f4785832647bdeddfba5e059e698251a4a658a99
Joaquim-Azevedo <joaquimazeveddoo@gmail.com>
2025-03-26 19:18:16 -0300

    Estilização melhorada no CSS e adição da função de desselecionar o número e poder apagá-lo e mudanças estruturais no código

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var easyBoard = [
   "9-4-7---2",
   "67-83----",
   "81--45---"
-]
+];
 
 var easySolution = [
   "387491625",
@@ -25,7 +25,7 @@ var easySolution = [
   "934176852",
   "675832941",
   "812945763"
-]
+];
 
 // var mediumBoard = [
 //   "---6-1---",
@@ -62,29 +62,29 @@ function setGame(){
     let number = document.createElement("div");
     number.id = i;
     number.innerText = i;
-    number.addEventListener('click', selectNumber)
-    number.classList.add("number")
-    document.getElementById("digits").appendChild(number)
+    number.addEventListener('click', selectNumber);
+    number.classList.add("number");
+    document.getElementById("digits").appendChild(number);
   }
   
     // Board (9x9)
     for (let r = 0; r < 9; r++){
       for (let c = 0; c < 9; c++){
-        let tile = document.createElement("div");
+        var tile = document.createElement("div");
         tile.id = r.toString() + "-" + c.toString();
         if(easyBoard[r][c] != "-"){
-          tile.innerText = easyBoard[r][c]
-          tile.classList.add('tile-start')
+          tile.innerText = easyBoard[r][c];
+          tile.classList.add('tile-start');
         }
         if (r == 2 || r == 5){
-          tile.classList.add('horizontal-line')
+          tile.classList.add('horizontal-line');
         }
         if (c == 5 || c == 2) {
-          tile.classList.add('vertical-line')
+          tile.classList.add('vertical-line');
         }
-        tile.addEventListener('click', selectTile)
-        tile.classList.add("tile")
-        document.getElementById("board").append(tile)
+        tile.addEventListener('click', selectTile);
+        tile.classList.add("tile");
+        document.getElementById("board").append(tile);
       }
     }
 }
@@ -93,33 +93,51 @@ function selectNumber(){
   if (numSelected != null){
     numSelected.classList.remove('number-selected')
   }
-
+  
   numSelected = this;
+  // console.log(numSelected.id)
   numSelected.classList.add('number-selected');
+  numSelected.addEventListener('click', deselectNumber);
+}
+
+// A function that will deselect the number and will allow the user to erase the placed number in the table.
+function deselectNumber(){
+  if(numSelected){
+    numSelected.removeEventListener('click', deselectNumber);
+    numSelected.classList.remove('number-selected');
+    numSelected = null
+  }
 }
 
 function selectTile(){
-  if(numSelected){
-    if(this.classList.contains('tile-start')){
-      return;
-    }
-    this.innerText = numSelected.id;
-    
-    // Coords = "0-0" "0-1", then 
-    let coords = this.id.split("-"); // => ["0", "0"]
-    let r = parseInt(coords[0]);
-    let c = parseInt(coords[1]);
+  // Coords = "0-0" "0-1", then 
+  let coords = this.id.split("-"); // => ["0", "0"]
+  let r = parseInt(coords[0]);
+  let c = parseInt(coords[1]);
 
+  if(this.classList.contains('tile-start')){
+    return;
+  } else if (numSelected == null){
+    this.innerText = "";
+    // console.log(coords)
+    return;
+  } else {
+    this.innerText = numSelected.id;
 
     // Checking if the number is correct, if it is, will display the number in the selected tile. If else, it will be displayed in red color.
     if (easySolution[r][c] == numSelected.id) {
-      this.classList.remove('wrong-number')
+      this.classList.remove('wrong-number');
     } else {
       errors += 1;
       document.getElementById("errors").innerText = errors;
-      let wrongcoordsnumber = r.toString() + '-' + c.toString()
+      let wrongcoordsnumber = r.toString() + '-' + c.toString();
       var wrongnumber = document.getElementById(wrongcoordsnumber);
       wrongnumber.classList.add('wrong-number');
     }
   }
+
+  // console.log(coords)
+
 }
+
+  
